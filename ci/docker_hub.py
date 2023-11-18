28cdb43aaed7da574259a9adf5dc75326c576aa2
Pekka Ranta-aho <pekka.ranta-aho@signify.com>
2023-11-18 12:32:56 +0100

    nigs and nags

--- ci/docker_hub.py
+++ ci/docker_hub.py
@@ -11,41 +11,23 @@ async def main():
     async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
         # Cache
         python_cache = client.cache_volume("python")
+        # set secret from argument
         password_argument = sys.argv[1]
         # set secret as string value
         secret = client.set_secret("password", password_argument)
-
-        entries = await client.host().directory(".").entries()
-        print("ENTRIES: ", entries)
-
         # create container
         source = (
             client.container()
             # .from_("python:3.11.0-slim-buster")
-            .from_("alpine:latest")
+            .from_("ubuntu:latest")
             .with_directory("/app", client.host().directory("."))
             .with_workdir("/app")
-            .with_exec(["/bin/sh", "-c", "'echo", "\"Hello World\""])
-            .with_exec(["/bin/sh", "-c", "'bash", "setup.sh'"])
-
-            # .with_exec(["ls", "."])
-            # .with_exec(["whoami"])
-            # .with_exec(["bash", "setup.sh"])
-
-            # .WithEnvVariable('PATH', '/root/.nvm/versions/node/v16.17.0/bin:$PATH')
-            # .with_exec(["pip", "install", "-r", "requirements.txt"])
-            # .with_exec(["pip", "install", "--upgrade", "openai==1.1.1"])
-            # .with_exec(["apt", "install", "curl", "-y"])
-
-            # .with_exec(["pwd"])
-            # .with_exec(["ls -la"])
-            # .with_exec(["whoami"])
-            # .with_exec(["./setup.sh"])
-            
-            # .with_entrypoint(
-            #     ["python3", "project_gin.py", "-t", "theme", "-p", "twitter"]
-            # )
-            # .with_mounted_cache("./py_cache", python_cache)
+            .with_exec(["echo", "hello world"])
+            .with_exec(["/bin/sh", "setup.sh"])
+            .with_entrypoint(
+                ["python3", "project_gin.py", "-t", "theme", "-p", "twitter"]
+            )
+            .with_mounted_cache("./py_cache", python_cache)
         )
 
         # use secret for registry authentication
@@ -53,7 +35,7 @@ async def main():
             "pecca86/poc:2"
         )
 
-    # print(f"Published at: {addr}")
+    print(f"Published at: {addr}")
 
 
 anyio.run(main)
