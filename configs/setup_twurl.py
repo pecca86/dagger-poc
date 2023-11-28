4973a53d227993269b88e154efbeecd971ff32a5
Pekka Ranta-aho <pekka.ranta-aho@signify.com>
2023-11-28 15:09:39 +0100

    new approach

---
+++ configs/setup_twurl.py
@@ -0,0 +1,24 @@
+import os
+
+file_path = "./.twurlrc"
+
+content = f"""
+---
+profiles:
+    gin_sipper:
+        {os.environ.get('TWITTER_CONSUMER_KEY')}:
+            username: {os.environ.get('TWITTER_USERNAME')}
+            consumer_key: {os.environ.get('TWITTER_CONSUMER_KEY')}
+            consumer_secret: {os.environ.get('TWITTER_CONSUMER_SECRET')} 
+            token: {os.environ.get('TWITTER_ACCESS_TOKEN')}
+            secret: {os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')}
+configuration:
+    default_profile:
+    - {os.environ.get('TWITTER_USERNAME')}
+    - {os.environ.get('TWITTER_CONSUMER_KEY')}
+bearer_tokens:
+    {os.environ.get('TWITTER_CONSUMER_KEY')}: {os.environ.get('TWITTER_BEARER_TOKEN')}"""
+
+with open(file_path, "w") as file:
+    file.write(content)
+    pass
