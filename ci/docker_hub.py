70cad394c57515bfbd34498bc1eedaf1add64336
Pekka Ranta-aho <pekka.ranta-aho@signify.com>
2023-11-28 14:34:24 +0100

    added )

--- ci/docker_hub.py
+++ ci/docker_hub.py
@@ -65,7 +65,7 @@ async def main():
                         "TWITTER_CONSUMER_KEY": os.environ.get("TWITTER_CONSUMER_KEY"),
                         "TWITTER_CONSUMER_SECRET": os.environ.get("TWITTER_CONSUMER_SECRET"),
                         "TWITTER_ACCESS_TOKEN": os.environ.get("TWITTER_ACCESS_TOKEN"),
-                        "TWITTER_ACCESS_TOKEN_SECRET": os.environ.get("TWITTER_ACCESS_TOKEN_SECRET",
+                        "TWITTER_ACCESS_TOKEN_SECRET": os.environ.get("TWITTER_ACCESS_TOKEN_SECRET"),
                         "TWITTER_BEARER_TOKEN": os.environ.get("TWITTER_BEARER_TOKEN"),
                     }
                 )
